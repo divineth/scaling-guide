039f30376d280d3f30a42fd326ca65139d674c9a
divineth <divinethomson@outlook.com>
2023-03-15 02:50:29 +0000

    Improve responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,11 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="font-comfortaa_reg text-5xl uppercase pb-4">Presale</h1>
-      <h3 className="font-lato_reg text-xl uppercase pb-6">Get your bread fresh out of the oven</h3>
-      <SaleWidget />
+      <h1 className="font-comfortaa_reg text-5xl uppercase pb-4 text-center">Presale</h1>
+      <h3 className="font-lato_reg text-xl uppercase pb-6 text-center">Get your bread fresh out of the oven</h3>
+      <div className="w-full flex justify-center px-4 sm:px-0">
+        <SaleWidget />
+      </div>
     </>
   );
 }
