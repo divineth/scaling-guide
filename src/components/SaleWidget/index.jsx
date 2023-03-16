ad4c355b252ecd9102a5d57c79ca370983574e01
divineth <divinethomson@outlook.com>
2023-03-16 23:53:31 +0000

    Update sale info

--- src/components/SaleWidget/index.jsx
+++ src/components/SaleWidget/index.jsx
@@ -72,14 +72,14 @@ function SaleWidget() {
         ? `${parseDecimals(utils.formatEther(userContribution), 2)} ETH`
         : "0 ETH",
     },
-    { title: "Hard Cap", value: "50 ETH" },
+    { title: "Hard Cap", value: "50.0 ETH" },
     {
       title: "Total Committed",
       value: totalCommitted
         ? `${parseDecimals(utils.formatEther(totalCommitted), 2)} ETH`
         : "0 ETH",
     },
-    { title: "Max per User", value: "1 ETH" }
+    { title: "Max per User", value: "1.0 ETH" }
   ];
 
   const barPercentage = 0;
